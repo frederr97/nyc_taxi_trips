a7da7eaa548a396e1298230c33ecd86e8250e675
Fred Resende <fredribeiro97@gmail.com>
2020-08-24 21:48:30 -0300

    Final commit

--- main.py
+++ main.py
@@ -9,34 +9,42 @@ def main():
     menu = None
     while menu != '0':
         os.system('cls')
-        print('1 - Distância média percorrida por viagens com no máximo 2 passageiros.')
-        print('2 - 3 maiores vendors em quantidade total de dinheiro arrecadado.')
-        print('3 - Histograma mensal de corridas pagas em dinheiro.')
-        print('4 - Série temporal em relação à quantidade de gorjetas diárias nos 3 últimos meses de 2012.')
-        print('5 - Tempo médio das corridas nos dias de sábado e domingo;.')
-        print('0 - Sair da aplicação.')
-        menu = input('\nDigite o número referente à análise desejada: ')
+        print('---------------------------------------------------------------------------------------')
+        print('                                   NYC Taxi Trips')
+        print('---------------------------------------------------------------------------------------\n\n')
+        print('1 - Average distance traveled by trips with a maximum of 2 passengers per year.')
+        print('2 - 3 biggest vendors in total amount of money raised.')
+        print('3 - Monthly histogram of races paid in cash.')
+        print('4 - Time series in relation to the amount of daily tips in the last 3 months of 2012.')
+        print('5 - Average race time on Saturday and Sunday per year.')
+        print('0 - Exit the application.')
+        menu = input('\nEnter the number for the desired analysis: ')
 
         if menu == '1':
+            print('\nWait while loading...')
             qt.question1() 
-            input('\nAperte qualquer botão para continuar...')
+            input('\nPress ENTER to continue...')
         elif menu == '2':
+            print('\nWait while loading...')
             qt.question2() 
-            input('\nAperte qualquer botão para continuar...')
+            input('\nAPress ENTER to continue...')
         elif menu == '3':
+            print('\nWait while loading...')
             qt.question3() 
-            input('\nAperte qualquer botão para continuar...')
+            input('\nPress ENTER to continue...')
         elif menu == '4':
+            print('\nWait while loading...')
             qt.question4() 
-            input('\nAperte qualquer botão para continuar...')
+            input('\nPress ENTER to continue...')
         elif menu == '5':
+            print('\nWait while loading...')
             qt.question5() 
-            input('\nAperte qualquer botão para continuar...')
+            input('\nPress ENTER to continue...')
         elif menu == '0':
             print('\nSaindo...')
         else:
-            print('\nOpção inválida.')
-            input('\nAperte qualquer botão para continuar...')
+            print('\nInvalid option.')
+            input('\nPress ENTER to continue...')
 
 
 if __name__ == '__main__':
--- questions.py
+++ questions.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
+import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 from matplotlib.ticker import StrMethodFormatter
@@ -11,8 +12,8 @@ import connection as conn
 def question1():
     cnxn = conn.sqlConnect()
     data = pd.read_sql('''select year(DROPOFF_DATETIME) YEAR, round(avg(TRIP_DISTANCE),3) AVERAGE from NYCTAXI.TRIP where PASSENGER_COUNT <= 2 group by year(DROPOFF_DATETIME)''', cnxn)
-    style.use('bmh')
-    data.plot(kind = 'bar', x = 'YEAR', y = 'AVERAGE', title ="Distância média percorrida por viagens com no máximo 2 passageiros", grid=False, xlabel = 'Year', ylabel = 'Average', legend=False, figsize=(9,6), color='#D8300C', zorder=2)
+    style.use('seaborn-deep')
+    data.plot(kind = 'bar', x = 'YEAR', y = 'AVERAGE', title ="Average distance traveled by trips with a maximum of 2 passengers per year", grid=False, xlabel = 'Year', ylabel = 'Average', legend=False, figsize=(9,6), color='#D8300C', zorder=2)
     plt.show()
 
 
@@ -21,27 +22,28 @@ def question2():
     data = pd.read_sql('''select top(3) V.NAME, round(sum(T.TOTAL_AMOUNT),2) TOTAL_AMOUNT from NYCTAXI.TRIP T inner join NYCTAXI.VENDOR V on T.VENDOR_ID = V.VENDOR_ID group by V.NAME order by round(sum(T.TOTAL_AMOUNT),2) desc''', cnxn)
     style.use('seaborn-deep')
     plt.rcParams.update({'font.size': 8, 'figure.autolayout': True})
-    data.plot(kind = 'bar', x = 'NAME', y = 'TOTAL_AMOUNT', title ="3 maiores vendors em quantidade total de dinheiro arrecadado", xlabel = 'Vendor', ylabel = 'Total', legend=False, figsize=(8,8), color='#13A9A8', zorder=2, width = 0.7) 
+    data.plot(kind = 'bar', x = 'NAME', y = 'TOTAL_AMOUNT', title ="3 biggest vendors in total amount of money raised", xlabel = 'Vendor', ylabel = 'Total amount', legend=False, figsize=(8,8), color='#13A9A8', zorder=2, width = 0.7) 
     plt.show()
 
 
 def question3():
     cnxn = conn.sqlConnect()
-    data = pd.read_sql('''select left(datename(month, DROPOFF_DATETIME),3) + '/' + cast(year(DROPOFF_DATETIME) as varchar) MONTH, count(1) QUANTITY from NYCTAXI.TRIP where PAYMENT_TYPE = 'Cash' group by left(datename(month, DROPOFF_DATETIME),3) + '/' + cast(year(DROPOFF_DATETIME) as varchar)''', cnxn)
-    data.plot(kind = 'bar', x = 'MONTH', y = 'QUANTITY', title ="Quantidade de corridas pagas em dinheiro mensalmente", grid=False, xlabel = 'Year', ylabel = 'Average', legend=False, figsize=(12,8), color='#8C19B1', zorder=2, width=1.0)    
+    data = pd.read_sql('''select DATEADD(month, DATEDIFF(month, 0, DROPOFF_DATETIME), 0) MONTH, count(1) QUANTITY from (select cast(DROPOFF_DATETIME as date) DROPOFF_DATETIME from NYCTAXI.TRIP where PAYMENT_TYPE = 'Cash') as AUX group by DATEADD(month, DATEDIFF(month, 0, DROPOFF_DATETIME), 0) order by DATEADD(month, DATEDIFF(month, 0, DROPOFF_DATETIME), 0)''', cnxn)
+    data['MONTH_YEAR'] = data['MONTH'].dt.to_period('M')
+    data.plot(kind = 'bar', x = 'MONTH_YEAR', y = 'QUANTITY', title ="Number of runs paid in cash monthly", grid=False, xlabel = 'Year/Month', ylabel = 'Quantity', legend=False, figsize=(12,8), color='#8C19B1', zorder=2, width=1.0)    
     plt.show()
 
 
 def question4():
     cnxn = conn.sqlConnect()
     data = pd.read_sql('''select cast(DROPOFF_DATETIME as date) DAY, round(sum(TIP_AMOUNT),2) TIP_AMOUNT from NYCTAXI.TRIP where year(DROPOFF_DATETIME) = 2012 and month(DROPOFF_DATETIME) in (10, 11, 12) group by cast(DROPOFF_DATETIME as date) order by cast(DROPOFF_DATETIME as date)''', cnxn)
-    data.plot(kind = 'line', x = 'DAY', y = 'TIP_AMOUNT', title ="Quantidade de gorjetas diárias nos últimos 3 meses de 2012", grid=False, xlabel = 'Day', ylabel = 'Tip amount', legend=False, figsize=(12,8), color='#86bf91', zorder=2)    
+    data.plot(kind = 'line', x = 'DAY', y = 'TIP_AMOUNT', title ="Number of daily tips in the last 3 months of 2012", grid=False, xlabel = 'Day', ylabel = 'Tip amount', legend=False, figsize=(12,8), color='#86bf91', zorder=2)    
     plt.show()
 
 
 def question5():
     cnxn = conn.sqlConnect()
-    data = pd.read_sql('''select year(DROPOFF_DATETIME) YEAR, avg(datediff(minute, PICKUP_DATETIME, DROPOFF_DATETIME)) MINUTES from NYCTAXI.TRIP where datename(dw, getdate()) in ('Saturday', 'Sunday') group by year(DROPOFF_DATETIME) order by year(DROPOFF_DATETIME) ''', cnxn)
-    data.plot(kind = 'bar', x = 'YEAR', y = 'MINUTES', title ="Tempo médio das corridas nos dias de sábado e domingo", grid=False, xlabel = 'Year', ylabel = 'Minutes', legend=False, figsize=(8,6), color='#E3E61E', zorder=2)    
+    data = pd.read_sql('''select year(DROPOFF_DATETIME) YEAR, avg(datediff(minute, PICKUP_DATETIME, DROPOFF_DATETIME)) MINUTES from NYCTAXI.TRIP where datename(dw, DROPOFF_DATETIME) in ('Saturday', 'Sunday') group by year(DROPOFF_DATETIME) order by year(DROPOFF_DATETIME)''', cnxn)
+    data.plot(kind = 'bar', x = 'YEAR', y = 'MINUTES', title ="Average race time on Saturday and Sunday per year", grid=False, xlabel = 'Year', ylabel = 'Minutes', legend=False, figsize=(8,6), color='#E3E61E', zorder=2)    
     plt.show()
 
